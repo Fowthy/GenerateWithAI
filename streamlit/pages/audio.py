f9193aac78bd433bc57aef5964de562c0e6b83fd
Fowthy <xfowth@gmail.com>
2024-01-24 18:32:59 +0100

    Updates to audio and video generation

--- streamlit/pages/audio.py
+++ streamlit/pages/audio.py
@@ -4,38 +4,45 @@ import replicate
 st.title("Realistic Audio Generator")
 st.subheader("an AI model that generates realistic audio")
 
-prompt = st.text_area("Enter your prompt here","Edo25 minor g melodies that sound dissonant and cinematic. Leading up to a crescendo that resolves in a 9th harmonic")
+# Radio button for model selection
+selected_model = st.sidebar.radio('Select Model', ["melody-large", "riffusion"])
 
-# Dropdowns
-top_k_options = [100, 250, 500]
-selected_top_k = st.selectbox('Select Top K', top_k_options)
-
-top_p_options = [0, 0.5, 1]
-selected_top_p = st.selectbox('Select Top P', top_p_options)
-
-duration_options = [10, 20, 30]
-selected_duration = st.selectbox('Select Duration', duration_options)
-
-temperature_options = [0.5, 1, 1.5]
-selected_temperature = st.selectbox('Select Temperature', temperature_options)
+# Text input for the prompt
+prompt = st.text_input("Enter the prompt here", "rock and roll song in the style of ACDC")
 
 if st.button('Generate Audio'):
     client = replicate.Client(api_token=st.secrets.replicate_api_token)
-    output = client.run(
-        "meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
-        input={
-            "top_k": selected_top_k,
-            "top_p": selected_top_p,
-            "prompt": prompt,
-            "duration": selected_duration,
-            "temperature": selected_temperature,
-            "continuation": False,
-            "model_version": "stereo-large",
-            "output_format": "wav",
-            "continuation_start": 0,
-            "multi_band_diffusion": False,
-            "normalization_strategy": "peak",
-            "classifier_free_guidance": 3
-        }
-    )
-    st.audio(output)  # Use st.audio() for audio files
+    
+    if selected_model == "melody-large":
+        output = client.run(
+            "meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
+            input={
+                "top_k": 250,
+                "top_p": 0,
+                "prompt": "Rock and Roll song in the style of ACDC",
+                "duration": 25,
+                "temperature": 1,
+                "continuation": False,
+                "model_version": "melody-large",
+                "output_format": "wav",
+                "continuation_start": 0,
+                "multi_band_diffusion": False,
+                "normalization_strategy": "peak",
+                "classifier_free_guidance": 3
+            }
+        )
+        st.audio(output)
+    elif selected_model == "riffusion":
+        output = client.run(
+            "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+            input={
+                "alpha": 0.5,
+                "prompt_a": "funky synth solo",
+                "prompt_b": "90's rap",
+                "denoising": 0.75,
+                "seed_image_id": "vibes",
+                "num_inference_steps": 50
+            }
+        ) 
+        st.audio(output['audio'])
+        st.image(output['spectrogram'])
--- streamlit/pages/video.py
+++ streamlit/pages/video.py
@@ -4,25 +4,21 @@ import replicate
 st.title("Realistic Video Generator")
 st.subheader("an AI model that generates realistic videos")
 
-prompt = st.text_area("Enter your prompt here","A video of Taylor Swift smiling")
+prompt = st.text_area("Enter your prompt here","A muscular man lifting heavy dumbbels in the gym, HD, 8k, realistic")
 
-# Dropdowns
-scheduler_options = ["EulerAncestralDiscreteScheduler"]
-selected_scheduler = st.selectbox('Select Scheduler', scheduler_options)
-
-steps_options = [10, 20, 30]
-selected_steps = st.selectbox('Select Number of Steps', steps_options)
 
 if st.button('Generate Video'):
     client = replicate.Client(api_token=st.secrets.replicate_api_token)
-    output = client.run(
-        "charlesmccarthy/hotshot-a40:12e43c65e07546ebe88cc1f15c44417fd95f9236d0fdc9b5559cbaa1b134c112",
+    output = replicate.run(
+        "lucataco/hotshot-xl:78b3a6257e16e4b241245d65c8b2b81ea2e1ff7ed4c55306b511509ddbfd327a",
         input={
-            "steps": selected_steps,
-            "width": 384,
-            "height": 672,
+            "mp4": False,
+            "seed": 6226,
+            "steps": 35,
+            "width": 672,
+            "height": 384,
             "prompt": prompt,
-            "scheduler": selected_scheduler,
+            "scheduler": "EulerAncestralDiscreteScheduler",
             "negative_prompt": "blurry"
         }
     )
