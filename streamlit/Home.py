9e333b866575495ad261b1e84a89fc7525fc8385
Fowthy <xfowth@gmail.com>
2024-01-24 18:57:49 +0100

    fix

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import replicate
 
-st.title("Wazzap")
-
+st.title("Realistic Content Generation with AI")
+st.write("This application is designed to explore the possibilities and limits of AI in the content creation world. It supports text, image, video and audio generation with just few simple clicks!")
--- streamlit/pages/video.py
+++ streamlit/pages/video.py
@@ -9,7 +9,7 @@ prompt = st.text_area("Enter your prompt here","A muscular man lifting heavy dum
 
 if st.button('Generate Video'):
     client = replicate.Client(api_token=st.secrets.replicate_api_token)
-    output = replicate.run(
+    output = client.run(
         "lucataco/hotshot-xl:78b3a6257e16e4b241245d65c8b2b81ea2e1ff7ed4c55306b511509ddbfd327a",
         input={
             "mp4": False,
