e9065dcb2e0c2de1b233a3d3e0efba566292cda6
Fowthy <xfowth@gmail.com>
2024-01-24 20:58:18 +0100

    update

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -1,5 +1,13 @@
 import streamlit as st
-import replicate
 
 st.title("Realistic Content Generation with AI")
 st.write("This application is designed to explore the possibilities and limits of AI in the content creation world. It supports text, image, video and audio generation with just few simple clicks!")
+
+st.header("Demo")
+st.video("demo.mp4")
+
+st.markdown("""Content Generation Capabilities 📚🎵
+- Text Generation: Leveraging advanced language models, generate creative and contextually relevant text.
+- Image Generation: Create unique and visually appealing images using state-of-the-art AI models.
+- Video Generation: Produce engaging and high-quality videos with the help of AI.
+- Audio Generation: From music to voice-overs, generate a wide range of audio content.""")
